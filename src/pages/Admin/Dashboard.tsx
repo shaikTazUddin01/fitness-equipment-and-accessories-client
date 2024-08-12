f1b4cbca1f629d55281c51a350c219e9b689ca71
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-08-12 23:05:53 +0600

    added react pieChart

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -1,32 +1,12 @@
+import Chart from "../../component/charts/Chart";
+
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-1 gap-2">
-      <progress
-        className="progress progress-accent w-56"
-        value={0}
-        max="100"
-      ></progress>
-      <progress
-        className="progress progress-accent w-56"
-        value="10"
-        max="100"
-      ></progress>
-      <progress
-        className="progress progress-accent w-56"
-        value="40"
-        max="100"
-      ></progress>
-      <progress
-        className="progress progress-accent w-56"
-        value="70"
-        max="100"
-      ></progress>
-      <progress
-        className="progress progress-accent w-56"
-        value="100"
-        max="100"
-      ></progress>
-    </div>
+   <div className="grid grid-cols-2">
+    <Chart></Chart>
+    <Chart></Chart>
+    <Chart></Chart>
+   </div>
   );
 };
 
